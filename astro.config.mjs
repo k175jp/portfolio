4cc83b954531c85d7a03fed2e940cb625c44161b
k175jp <tai.sp.tai@gmail.com>
2026-05-26 01:35:44 +0900

    [add] github settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://k175jp.github.io",
+  base: "/portfolio",
+  integrations: [],
   vite: {
     plugins: [tailwindcss()]
   }
